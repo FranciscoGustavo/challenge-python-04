a2a7d1498d6eda9ef3759b66f4c914911238450a
FrcGustavo <hidalgo_fco520@outlook.com>
2020-04-22 18:05:44 -0500

    decorators are hard

--- html_decorators.py
+++ html_decorators.py
@@ -1,22 +1,28 @@
 def div(func):
-    # You have to code here!
-    pass
+    def wrapper(nombre):
+        return '<div>' + func(nombre) + '</div>'
+
+    return wrapper
 
 
 def article(func):
-    # You have to code here!
-    pass
+    def wrapper(nombre):
+        return '<article>' + func(nombre) + '</article>'
+
+    return wrapper
 
 
 def p(func):
-    # You have to code here!
-    pass
+    def wrapper(nombre):
+        return '<p>' + func(nombre) + '</p>'
+
+    return wrapper
 
 
 # Here you must apply the decorators, uncomment this later
 # @div
 # @article
-# @p
+@p
 def saludo(nombre):
     return f'¡Hola {nombre}, ¿Cómo estás?'
 
